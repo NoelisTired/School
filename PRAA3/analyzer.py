9b6b3c331e704a88d2f4b5d2e12de3874b0cfd58
NoelP <D298110@edu.curio.nl>
2022-11-29 22:28:13 +0100

    DONE

--- PRAA3/analyzer.py
+++ PRAA3/analyzer.py
@@ -1,4 +1,4 @@
-import csv, datetime, os, time
+import csv, datetime, time, os
 
 class Functions:
     def __init__(self):
@@ -14,14 +14,39 @@ class Functions:
     def avgsalary():
         allsal = [int(i) for i in [row['Salaris_bruto'] for row in Functions().file]] # list comprehension
         return(sum(allsal)/len(allsal))
-    
+    @staticmethod
+    def departmentMembers(department):
+        return [row['Voornaam'] for row in Functions().file if row['Functie'] == department]
+
+    @staticmethod
+    def advDepartmentFunctions(departmens, job):
+        return [row['Voornaam'] for row in Functions().file if row['Functie'] == job and row['Afdeling'] in departmens]
+
     @staticmethod
     def departmentsalary(department=None):
         allsal = [int(i) for i in [row['Salaris_bruto'] for row in Functions().file if row['Functie'] == department]] # list comprehension
         return(sum(allsal)/len(allsal))
+
     @staticmethod
     def retireAlert():
-        return [row['Voornaam'] for row in Functions().file if datetime.datetime.strptime(row['Geboortedatum'], '%d-%m-%Y').year + 65 < datetime.datetime.now()]
+        #! Ervanuitgaand dat je op je 70e met pensioen mag en dus met 68 rekent
+        return [row['Voornaam'] for row in Functions().file if Functions.age(datetime.datetime.strptime(row['Geboortedatum'], "%d-%m-%Y")) >= 68]
+    
+    @staticmethod
+    def age(born):
+        today = datetime.date.today()
+        return(today.year - born.year - (\
+            (today.month, today.day) < (born.month, born.day)\
+            )\
+        )
+    @staticmethod
+    def leaderboard(limit=None):
+        print("Employee Leaderboard (Top 10)")
+        x = [[row['Datum_in_dienst,'].replace(",",""), row['Voornaam']] for row in Functions().file]
+        x.sort(key=lambda date: datetime.datetime.strptime(date[0], "%d-%m-%Y"))
+        return [(f"[{i}]","Employee:",v[1],"Has been working Since", v[0]) for i,v in enumerate(x, start=1) if i <= limit]
+        
+
     @staticmethod
     def DEV():
         while True:
@@ -31,7 +56,7 @@ class Menu:
     def __init__(self):
         self.menu = {
             '1': 'Average salary',
-            '2': 'Average salary per department',
+            '2': 'Average salary for Function',
             '3': 'No. of employees retiring within 2 years',
             '4': 'No. of employees with function "Chaffeur"',
             '5': 'Top 10 longest employed employees',
@@ -44,10 +69,20 @@ class Menu:
         if choice == '1':
             print('Average salary:', Functions.avgsalary())
         elif choice == '2':
-            dep = input('Enter department: \n')
-            print('Average salary for department:', Functions.departmentsalary(dep))
+            dep = input('Enter Function: \n')
+            print('Average salary for Function:\n$', Functions.departmentsalary(dep))
         elif choice == '3':
-            print("No. of employees retiring within 2 years:", len(Functions.retireAlert()))
+            # ! Development! \ \ > print(Functions.retireAlert())
+            print("No. of employees retiring within 2 years:\n", len(Functions.retireAlert()))
+        elif choice == '4':
+            print('No. of employees with function "Chaffeur":\n', len(Functions.departmentMembers('Chauffeur')))
+        elif choice == '5':
+            Functions.leaderboard(10)
+        elif choice == '6':
+            dep = input('Enter department: \n')
+            job = input('Enter job: \n')
+            print("People per department with function:\n")
+            [print(i,v) for i,v in enumerate(Functions.advDepartmentFunctions(dep, job), start=1)]
         elif choice == '7':
             exit()
         elif choice == '8':
@@ -55,6 +90,10 @@ class Menu:
         else:
             print('Not implemented yet')
 if __name__ == '__main__':
-    [print(f'{k}: {v}') for k, v in Menu().menu.items()]
-    choice = input('\nChoose an option: \n')
-    Menu.eventHandler(choice) if choice.isdigit() and int(choice) <= 8 else print('Invalid input')
+    while True:
+        os.system('cls') if os.name == 'nt' else os.system('clear')
+        print(f"\t[Welcome to the Employee Analyzer]\n")
+        [print(f'{k}: {v}') for k, v in Menu().menu.items()]
+        choice = input('\nChoose an option: \n')
+        Menu.eventHandler(choice) if choice.isdigit() and int(choice) <= 8 else print('Invalid input')
+        time.sleep(3)
--- PRAA3/playground.py
+++ PRAA3/playground.py
@@ -5,4 +5,4 @@ def calculate_age(born):
     today = date.today()
     print(today.year - born.year - ((today.month, today.day) < (born.month, born.day)))
 
-print(calculate_age(datetime.date(1969, 8, 12)))
+print(calculate_age(datetime.date(1949, 6, 24)))
