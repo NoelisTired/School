ed043e9a43fc6741f00d66b69b60473adb111724
NoelP <nmjj.populiers@gmail.com>
2022-12-12 21:13:09 +0100

    fart

--- WEB Sites/H10/index.php
+++ WEB Sites/H10/index.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
     <title>Vragenlijst</title>
 </head>
 <body>
     <main>
         <h1>Vragenlijst</h1>
-        <form action="index.php" class="form" method="post">
+        <form action="backend/controller.php" class="form" method="post">
             <div class="form-group">
                 <label for="Reset">Reset Form</label>
                 <input type="submit" name="reset" value="Reset">
